aec34f38d363104eed3f3ade0bcb5e39a445dba4
Japneet Singh <japneetsingh1807@outlook.it>
2024-08-30 11:09:11 +0200

    More exc

--- exerciseCalculator.ts
+++ exerciseCalculator.ts
@@ -13,18 +13,18 @@ const calculateExercises = (dailyExercisesHours: number[], targetAmount: number)
   const trainingDays = dailyExercisesHours.filter(dh => dh !== 0).length;
   const totalHours = dailyExercisesHours.reduce((t, h) => t + h, 0);
   const targetHours = targetAmount * periodLength;
-  const success = targetHours <= totalHours
+  const success = targetHours <= totalHours;
   
   const rating = (() => {
     if (success) {
-      if (totalHours - targetHours > 10) return 5
-      if (totalHours - targetHours > 5) return 4
-      return 3
+      if (totalHours - targetHours > 10) return 5;
+      if (totalHours - targetHours > 5) return 4;
+      return 3;
     }
-    if (targetHours - totalHours < 5) return 2
-    return 1
+    if (targetHours - totalHours < 5) return 2;
+    return 1;
   })();
-  console.log(rating)
+
   let ratingDescription;
 
   switch (rating) {
@@ -38,10 +38,10 @@ const calculateExercises = (dailyExercisesHours: number[], targetAmount: number)
       ratingDescription = 'target reached, good job!';
       break;
     case 4:
-      ratingDescription = 'Nice, you reached the target and went beyond!'
+      ratingDescription = 'Nice, you reached the target and went beyond!';
       break;
     case 5:
-      ratingDescription = 'Kudos! You made the target and worked very hard, continue like this and sky is the limit!'
+      ratingDescription = 'Kudos! You made the target and worked very hard, continue like this and sky is the limit!';
   }
 
   const average = totalHours / periodLength;
@@ -54,13 +54,13 @@ const calculateExercises = (dailyExercisesHours: number[], targetAmount: number)
     ratingDescription,
     target: targetAmount,
     average
-  }
-}
+  };
+};
 
-let excPerDays = [ ...process.argv ]
+const excPerDays = [ ...process.argv ];
 
-excPerDays.shift()
-excPerDays.shift()
+excPerDays.shift();
+excPerDays.shift();
 
-const target = excPerDays.shift()
-console.log(calculateExercises(excPerDays.map(n => parseInt(n, 10)), Number(target)))
+const target = excPerDays.shift();
+console.log(calculateExercises(excPerDays.map(n => parseInt(n, 10)), Number(target)));
--- index.ts
+++ index.ts
@@ -1,8 +1,21 @@
 import express from 'express';
+import calculateBmi from './calculateBmi';
+
 const app = express();
 
 app.get('/hello', (_req, res) => {
   res.send('Hello Full Stack!');
+});
+
+app.get('/bmi', (req, res) => {
+  if (!req.query.height || !req.query.weight) return res.status(400).json({
+    error: "malformatted parameters"
+  });
+  return res.json({
+    height: Number(req.query.height),
+    weight: Number(req.query.weight),
+    bmi: calculateBmi(Number(Number(req.query.height)), Number(req.query.weight)) as string
+  });
 });
 
 const PORT = 3003;
